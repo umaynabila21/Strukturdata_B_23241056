cce7215e80f48727c13344ff170f67281d520972
umaynabila21 <146051177+umaynabila21@users.noreply.github.com>
2024-04-22 11:12:27 +0700

    22_04_2024

---
+++ struktur data/meet3.py
@@ -0,0 +1,22 @@
+#Contoh Tipe data (integer) : Angka satuan bulat
+bil_integer = 1
+print(type(bil_integer))
+
+print ("type data dari bil_integer adalah :", type(bil_integer))
+
+#Contoh Tipe data (float) : Bilangan Desimal
+bil_float = 2.5
+print ("type data dari bil_float adalah :", type(bil_float))
+
+#Contoh Tipe data (string) : Kumpulan Karakter
+bil_string = "Hai"
+print ("type data dari bil_string adalah :", type(bil_string))
+
+#Contoh Tipe data (bolean) : biner, 0,1, True False
+bil_bol = False
+print ("type data dari bil_bol adalah :", type(bil_bol))
+
+#Tipe data dari Bahasa C
+from ctypes import c_double
+bil_double = c_double(20000000)
+print ("type data dari bil_double adalah :", type(bil_double))
