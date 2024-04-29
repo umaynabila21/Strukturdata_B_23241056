d6ce851a26f040c7d51dc1bbb5b6791e887ddafc
umaynabila21 <146051177+umaynabila21@users.noreply.github.com>
2024-04-29 11:03:39 +0700

    29_04_2024

---
+++ struktur data/meet4&5.py
@@ -0,0 +1,65 @@
+# Inputan dari user
+# aritmatika
+
+# belajar inputan
+#mahmud = input("masukan kata : ")
+#print("isi dari mahmud :", mahmud, "bertipe data :", type {mahmud}
+      
+# belajar aritmatika dasar
+x = 11
+y = 4
+
+# penjumlahan +
+hasil = x + y
+print ("x + y =",hasil)
+
+# pengurangan -
+hasil = x - y
+print ("x - y =",hasil)
+
+# perkalian x
+hasil = x * y
+print ("x * y =",hasil)
+
+# pembagian /
+hasil = x / y
+print ("x : y =",hasil)
+
+# perpangkatan exponen **
+hasil = x ** y
+print ("x ^ y =",hasil)
+
+# modulus % 
+hasil = y % x 
+print (" x mod y =" ,hasil )
+
+# floor division (pembulatan kebawah) //
+hasil = x // y
+print (" x // y =" , hasil)
+
+# aritmatika prioritas
+# (), exponen, perkalian/pembagian, penjumlahan/pengurangan
+x = 3
+y = 4
+z = 5
+
+hasil = ( x ** y * z + x / y - z % x // y)
+print(hasil)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
