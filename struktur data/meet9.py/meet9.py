35656eb1cad2261ca17cfe97122b6c9ba0a84fd5
umaynabila21 <146051177+umaynabila21@users.noreply.github.com>
2024-06-03 11:54:35 +0800

    03/06/2024

---
+++ struktur data/meet9.py/meet9.py
@@ -0,0 +1,23 @@
+# percabangan jika maka
+
+name = input("what is your name : ")
+#if kondisi : aksi
+
+#if standar
+#if name == "Nabila" : print("Mahasiswa Undikma")
+#print("end of program")
+
+#2. if indentasi
+if name=="nabila":
+    print("mahasiswa undikma")
+    print("kelas pti b")
+print("end of program")
+
+# 3. IF ELSE
+if name=="nabila":
+    print("mahasiswa undikma")
+    print(f"terimakasih {name}")
+else:
+     print("anda bukan mahasiswa")
+print("end of program")
+
